d1a187bded5727c901d3303c12a369f7c61e3929
David Fry <david.fry@modusbox.com>
2023-05-20 14:37:07 +0100

    cleanup env vars

---
+++ cleanup.sh
@@ -0,0 +1,5 @@
+rm -rf iac-modules/
+rm -rf output/*
+rm -rf /root/.ansible/
+rm -rf /tmp/archivedhttpstate.sh /tmp/backup-deploy.tfstate /tmp/bootstrap/ /tmp/citemplates/
+unset IAC_TERRAFORM_MODULES_TAG
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-#echo "https://${PRIVATE_REPO_USER}:${PRIVATE_REPO_TOKEN}@github.com" > ~/.gitcredentials.store
-#git config --global credential.helper 'store --file ~/.gitcredentials.store'
-#git config --global advice.detachedHead false
+echo "https://${PRIVATE_REPO_USER}:${PRIVATE_REPO_TOKEN}@${PRIVATE_REPO}" > ~/.gitcredentials.store
+git config --global credential.helper 'store --file ~/.gitcredentials.store'
+git config --global advice.detachedHead false
 git clone https://github.com/mojaloop/iac-modules.git
 cd iac-modules
-git checkout $IAC_MODULES_TAG
+git checkout $IAC_TERRAFORM_MODULES_TAG
