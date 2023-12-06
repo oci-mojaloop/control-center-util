0f92f46aa377a41bb1de310b18b33df9860f7982
Deepak Devadathan <deepak.devadathan@oracle.com>
2023-12-06 04:56:17 +0000

    added variable for iac repo
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 echo "https://${PRIVATE_REPO_USER}:${PRIVATE_REPO_TOKEN}@${PRIVATE_REPO}" > ~/.gitcredentials.store
 git config --global credential.helper 'store --file ~/.gitcredentials.store'
 git config --global advice.detachedHead false
-git clone https://github.com/mojaloop/iac-modules.git
+git clone $IAC_TERRAFORM_MODULES_REPO
 cd iac-modules
 git checkout $IAC_TERRAFORM_MODULES_TAG
